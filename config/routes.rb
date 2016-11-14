888c66cfcae4f29bd8fe8bf52058c3eadf9288af
Notika314 <notika314@gmail.com>
2016-11-14 14:52:47 -0500

    working on comments'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :users
+  resources :users do 
+    member do 
+      get 'posts' => 'users#posts'
+    end
+  end
   resources :posts
+  resources :posts do 
+    resources :comments 
+    end
+  end
+
+  root to: 'posts#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
